92d5197d6454b49430a1c4afdbea00b1ef80de76
Jose Nieto <inge1neuro@gmail.com>
2021-08-10 10:57:30 -0500

    Integrando tema adminlte.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+    <script src="https://use.fontawesome.com/eb01d11666.js"></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
